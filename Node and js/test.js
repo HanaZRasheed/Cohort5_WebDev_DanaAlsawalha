ddb78e71340d71c497da5e8459d344311263e19e
Danaalsawalha <danaalsawalha1998@gmail.com>
2020-09-07 10:48:40 +0300

    Node and js

---
+++ Node and js/test.js
@@ -0,0 +1,11 @@
+var a = 4;
+var b = 6;
+
+function myFunction() {
+    return a + b;
+  }
+
+function myFunction2(text){
+    var val = document.getElementById("textInp").value;
+    document.getElementById("text").innerHTML = val;
+}
