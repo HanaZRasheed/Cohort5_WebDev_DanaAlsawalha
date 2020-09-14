a126f1baed0c71e8e5a746b6214f1d2187ad2e03
Danaalsawalha <danaalsawalha1998@gmail.com>
2020-09-14 07:04:13 +0300

    serverSideTasks

---
+++ serverSideTasks/task1/index_JSON.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var url = require('url');
+
+const hostname = 'localhost';
+const port = 8084;
+
+var server = http.createServer((request, response) => {
+    //processing GET paramters
+    var query = url.parse(request.url, true).query;
+    var num1 = query.num1;
+    var num2 = query.num2;
+    var num3 = query.num3;
+    var result = {
+        "num1":num1,
+        "num2":num2,
+        "num3":num3,
+    };
+
+    //Build HTTP response
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Cache-Control', 'no-store');
+
+    response.write(`num1= ${num1}, num2= ${num2}, num3= ${num3}`);
+    response.write(JSON.stringify(result));
+    response.end();
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
